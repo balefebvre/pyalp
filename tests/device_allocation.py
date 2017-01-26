036d7365f0f3e48ccf16d6b9418fd473a3a6b7bd
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2017-01-26 23:46:40 +0100

    Rename test of device allocation

---
+++ tests/device_allocation.py
@@ -0,0 +1,10 @@
+import pyalp as alp
+
+
+
+# Allocate device
+dev = alp.device.allocate()
+
+# Print device number and identifier
+print("  Device number    : {}".format(dev.nb))
+print("  Device identifier: {}".format(dev.id))
